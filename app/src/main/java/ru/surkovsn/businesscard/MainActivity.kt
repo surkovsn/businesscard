e2d493cb6babc3218a0693ba9b107b477eefbd44
surkov_sn <surkov_sn@inbox.ru>
2024-06-09 21:54:49 +0500

    Add fragment_start.xml and fragment_card.xml

--- app/src/main/java/ru/surkovsn/businesscard/MainActivity.kt
+++ app/src/main/java/ru/surkovsn/businesscard/MainActivity.kt
@@ -15,12 +15,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.tvHW.text = getString(R.string.hwtext)
+
 
     }
 }
