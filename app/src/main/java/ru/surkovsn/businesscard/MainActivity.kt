dd242b2a55822771e8bc323b62d15ad9ffe333d3
surkov_sn <surkov_sn@inbox.ru>
2024-06-09 16:54:02 +0500

    intt

---
+++ app/src/main/java/ru/surkovsn/businesscard/MainActivity.kt
@@ -0,0 +1,26 @@
+package ru.surkovsn.businesscard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.surkovsn.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.tvHW.text = getString(R.string.hwtext)
+
+    }
+}
